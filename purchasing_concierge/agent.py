500b4fdb8c0f8144cf469ec319eeee6e28c6e4bd
PriyaBSavithiri <priya.savithiri@multicorewareinc.com>
2025-07-29 09:56:34 +0000

    Workflow of Burger-Root agent

--- purchasing_concierge/agent.py
+++ purchasing_concierge/agent.py
@@ -23,6 +23,6 @@ load_dotenv()
 root_agent = PurchasingAgent(
     remote_agent_addresses=[
         # os.getenv("PIZZA_SELLER_AGENT_URL", "http://localhost:10000"),
-        # os.getenv("BURGER_SELLER_AGENT_URL", "http://localhost:10001"),
+        os.getenv("BURGER_SELLER_AGENT_URL", "http://localhost:10003"),
     ]
 ).create_agent()
